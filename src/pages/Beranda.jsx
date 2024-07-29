f36a95471b35d8670f5f8e87e3ce39dd5fe72244
Sahra-Fatimah-Yasir <2101020084@student.umrah.ac.id>
2024-07-29 13:55:11 +0700

    style : fix edit beranda

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -7,7 +7,7 @@ const userName = "Nama Pengguna"; // Replace this with actual method to retrieve
 export default function Beranda() {
   return (
     <div className="p-6 bg-gray-50 min-h-screen">
-      <h1 className="text-3xl font-bold mb-4 text-blue-950">Beranda 2</h1>
+      <h1 className="text-3xl font-bold mb-4 text-blue-950">Beranda</h1>
       <div className="bg-white p-6 rounded-lg shadow-md max-w-5xl mx-auto">
         <h2 className="text-2xl font-semibold mb-2 text-gray-800">Selamat Datang, {userName}!</h2>
         <p className="text-md text-gray-500">Anda dapat melakukan absensi, mencatat kegiatan harian, dan melihat riwayat absen dan riwayat catatan kegiatan.</p>
