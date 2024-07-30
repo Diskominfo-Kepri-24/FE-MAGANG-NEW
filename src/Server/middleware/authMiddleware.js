cf46099d187aa680d2c5842df990778120210a68
Sahra-Fatimah-Yasir <2101020084@student.umrah.ac.id>
2024-07-30 10:04:58 +0700

    style : delete server directory

--- src/Server/middleware/authMiddleware.js
+++
@@ -1,9 +0,0 @@
-const authMiddleware = (req, res, next) => {
-    if (!req.session.userId) {
-      return res.status(401).send('Unauthorized');
-    }
-    next();
-  };
-  
-  module.exports = authMiddleware;
-  
